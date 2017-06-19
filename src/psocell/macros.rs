c7c3f376a2c40c6e44c39c598804ba5d1ba15562
Alex Butler <alexheretic@gmail.com>
2017-06-19 10:56:58 +0100

    Reduce repeatition in macro code

--- src/psocell/macros.rs
+++ src/psocell/macros.rs
@@ -12,29 +12,6 @@ macro_rules! debug_watcher_pso_cell {
     (pipe = $pipe_name:ident,
     vertex_shader = $vs:expr,
     fragment_shader = $fs:expr,
-    factory = $factory:expr) => {{
-        use std::path::Path;
-        use $crate::WatcherPsoCellBuilder;
-
-        match Path::new(file!()).canonicalize() {
-            Ok(path) => match path.parent().ok_or("Could not find current dir") {
-                Ok(dir) => {
-                    let vs = $vs.split("/").fold(dir.to_path_buf(), |path, s| path.join(s));
-                    let fs = $fs.split("/").fold(dir.to_path_buf(), |path, s| path.join(s));
-                    WatcherPsoCellBuilder::using($pipe_name::new())
-                        .vertex_shader(vs)
-                        .fragment_shader(fs)
-                        .build($factory)
-                },
-                Err(err) => Err(err.into())
-            },
-            Err(err) => Err(err.into())
-        }
-    }};
-
-    (pipe = $pipe_name:ident,
-    vertex_shader = $vs:expr,
-    fragment_shader = $fs:expr,
     factory = $factory:expr,
     $($opt:ident = $opt_val:expr),+) => {{
         use std::path::Path;
@@ -55,6 +32,18 @@ macro_rules! debug_watcher_pso_cell {
             },
             Err(err) => Err(err.into())
         }
+    }};
+
+    (pipe = $pipe_name:ident,
+    vertex_shader = $vs:expr,
+    fragment_shader = $fs:expr,
+    factory = $factory:expr) => {{
+        use gfx::Primitive;
+        debug_watcher_pso_cell!(pipe = $pipe_name,
+                                vertex_shader = $vs,
+                                fragment_shader = $fs,
+                                factory = $factory,
+                                primitive = Primitive::TriangleList)
     }};
 }
 
